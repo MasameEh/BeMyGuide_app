95449ecb05be16d173c4d5ec4d3bbf21d52bdae7
MasameEh <samehms116@gmail.com>
2023-02-06 02:21:47 +0200

    added last changes

---
+++ lib/modules/blind_features/blindfeatures_screen.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/material.dart';
+
+class BlindFeaturesScreen extends StatelessWidget {
+  const BlindFeaturesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blind features'),
+      ),
+
+    );
+  }
+}
