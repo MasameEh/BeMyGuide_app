06b89d4b64cb75c3e43acbba6004ec967cb99938
Moahmed Elgebaly <elgebalym2082000@gmail.com>
2023-02-20 18:46:35 +0200

    tf

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
             debugShowCheckedModeBanner: false,
-            home: startWidget,
+            home: BlindFeaturesScreen(),
           );
         },
       ),
