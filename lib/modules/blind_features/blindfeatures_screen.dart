f54782d397b8a90efcbc59597f49800ee37fa8a2
Moahmed Elgebaly <elgebalym2082000@gmail.com>
2023-07-09 03:33:55 +0300

    text recognation feature adding

--- lib/modules/blind_features/blindfeatures_screen.dart
+++ lib/modules/blind_features/blindfeatures_screen.dart
@@ -4,19 +4,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:graduation_project/modules/speech/speechscreen.dart';
 import 'package:graduation_project/modules/video_Call/join_screen.dart';
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart';
-import 'package:share_plus/share_plus.dart';
-import 'package:file_picker/file_picker.dart';
-
 import '../../layout/eyes_app/cubit/cubit.dart';
 import '../../layout/eyes_app/cubit/states.dart';
 import '../../shared/components/components.dart';
 import '../bluetooth/bluetooth.dart';
 import '../settings/settings_screen.dart';
+import 'text_recognation_screen.dart';
 
 class BlindFeaturesScreen extends StatefulWidget {
   const BlindFeaturesScreen({super.key});
@@ -150,35 +143,53 @@ class _MyWidgetState extends State<BlindFeaturesScreen> {
                                     const SizedBox(
                                       width: 7,
                                     ),
-                                     Text('Send a pic',
+                                     Text('Glasses Connection',
                                       style:  GoogleFonts.acme(
                                         color: Color.fromARGB(255, 180, 31, 87),
                                         fontSize: 30.0,
                                       ),
                                      ),
-                                        // style: TextStyle(
-                                        //   fontSize: 25,
-                                        //   color:
-                                        //       Color.fromARGB(255, 180, 31, 87),
-                                        //   fontWeight: FontWeight.bold,
-                                        // )),
+                                       
                                   ],
                                 ),
                               ),
                             ),
                           ),
-                          // defaultButton(
-                          //   width: 300.0,
-                          //   radius: 20.0,
-                          //   height: 60.0,
-                          //   borderColor: Colors.black,
-                          //   function: () {
-                          //     navigateTo(context, bluetoothScreen());
-                          //   },
-                          //   textColor: Color.fromARGB(255, 180, 31, 87),
-                          //   text: 'Bluetooth',
-                          //   background: Colors.white,
-                          // ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          GestureDetector(
+                            onTap: (){
+                              navigateTo(context, TextRecognation());
+                            },
+                            child: Container(
+                              width: MediaQuery.of(context).size.width*0.9,
+                              height: 60.0,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.white,
+                                  border: Border.all(color: Colors.black.withOpacity(.4)),
+                                ),
+                            child: Center(
+                              child: Row(
+                                children: [
+                                  SizedBox(width: 7,),
+                                  Icon(Icons.text_fields,
+                                    color: Colors.grey.withOpacity(.6),
+                                    size: 30,
+                                  ),
+                                  SizedBox(width: 7,),
+                                  Text('Text Recognition',
+                                      style:  GoogleFonts.acme(
+                                        color: Color.fromARGB(255, 180, 31, 87),
+                                        fontSize: 30.0,
+                                      ),)
+                                ],
+                              ),
+                            ),
+                          ),
+                          ),
+                         
                           SizedBox(
                             height: 15.0,
                           ),
